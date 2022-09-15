2ac3e63c3d3857a3e92b66bf7516df4be100b34d
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-09-15 22:33:00 +0100

    Deploy my Docker Image on Azure Container Instance

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const config = require("./config");
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Welcome to the World of Containers !");
+  res.send("<h1>Welcome to the World of Containers on Azure Cloud !</h1>");
+});
+
+app.get("/youssef", (req, res) => {
+  res.send("Hello Youssef, How are you doing Today !");
 });
 
 app.listen(config.PORT, () => {
